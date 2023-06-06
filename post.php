ff515e0a8e9f14b38845fc2726b9808b2d424d72
Thalita Thie <thalita.akayama@outlook.com>
2023-06-06 16:20:30 -0300

    estilo

--- post.php
+++ post.php
@@ -1,12 +1,25 @@
+<html>
+<head>
+<title>Post</title>
+<link rel="stylesheet" type="text/css" href="estilo.css">
+</head>
+<body>
+    <h1><nomeblog><b>Nome do blog</b></nomeblog> </h1>
+
+<!-- Conteúdo -->
+
+<p style="text-align: center;">Bem vindo! Este é um blog teste.</p>
+<img src="https://veolink.com.br/wp-content/uploads/2021/05/24-1.jpg" style="width:900px; display: block; margin-left: auto; margin-right: auto;">
+
 <?php
-$titulo="<h1>Título post 1</h1>";
+$titulo="<h2>Título post 1</h2>";
 $conteudo='<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
-<h2>Where does it come from?</h2>
+<h3>Where does it come from?</h3>
 <p>Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, "Lorem ipsum dolor sit amet..", comes from a line in section 1.10.32.</p>';
 
-$titulo2="<h1>Título post 2</h1>";
+$titulo2="<h2>Título post 2</h2>";
 $conteudo2='<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
-<h2>Where does it come from?</h2>
+<h3>Where does it come from?</h3>
 <p>Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, "Lorem ipsum dolor sit amet..", comes from a line in section 1.10.32.</p>';
 
 $idpost=$_GET["idpost"];
@@ -28,15 +41,8 @@ $postagens = array($post1, $post2);
     }
 ?>
 
-<html>
-<head>
-<title>Post</title>
-</head>
-<body>
-
-<h1><b>    
-<?=$titulo?></b></h1>
-<p>Isso é um teste</p>
-
+<footer>
+    <p>Rodapé da página</p>
+  </footer>
 </body>
 </html>
