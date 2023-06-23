acb7cd9c981567e2092f741ad03290723ca550f1
Thalita Thie <thalita.akayama@outlook.com>
2023-06-23 15:53:48 -0300

    post delete

--- post.php
+++ post.php
@@ -12,19 +12,22 @@ if ($result->num_rows > 0) {
 ?>
 
 <p style="text-align: center;">
-<a href="editpost.php?idpost=<?=$row["id"]?>" class="botao">Editar post</a>
-<br><a href="post_delete.php?idpost=<?=$row["id"]?>" class="botao">Excluir post </a>
+<a href="editpost.php?idpost=<?=$row["id"]?>"><button>Editar post</button></a> 
+<button onclick="alertDeletePost()">Excluir post</button>
 </p>
 
+<script>
+function alertDeletePost() {
+if (confirm('Tem certeza que deseja excluir esta postagem?') == true)
+window.location.href = "post_delete.php?idpost=<?=$row["id"]?>";
+}
+</script>
 
 <?php
     }    
         }
 $conn->close();
-
 ?>
-
-
 
 <?php
 /* $titulo="<h2>Título post 1</h2>";
@@ -50,41 +53,7 @@ $conteudo4='<img class="user-photo" src="img/img4.jpg" style="width:900px; displ
 <p>Iniício do conteúdo 4: Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
 <h3>Where does it come from?</h3>
 <p>Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, "Lorem ipsum dolor sit amet..", comes from a line in section 1.10.32.</p>';
- 
-    $idpost=$_GET["idpost"]; 
-
-$postagens = array(
-    array(
-        'id' => 1,
-        'titulo' => $titulo,
-        'conteudo' => $conteudo
-    ),
-    array(
-        'id' => 2,
-        'titulo' => $titulo2,
-        'conteudo' => $conteudo2
-    ),
-    array(
-        'id' => 3,
-        'titulo' => $titulo3,
-        'conteudo' => $conteudo3
-    ),
-    array(
-        'id' => 4,
-        'titulo' => $titulo4,
-        'conteudo' => $conteudo4
-    )
-);
-    
-foreach ($postagens as $postagem) {
-// print_r($postagem); // 
-// echo "id antes do if:" . $postagem['id'];
-    if ($idpost == $postagem['id']) {
-        echo $postagem['titulo'] . '<br>' . $postagem['conteudo'];
-    }
-}
-*/
+ */
 
 include 'footer.php';
-
 ?>
